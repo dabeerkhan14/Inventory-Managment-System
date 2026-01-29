e123663b11e9270aece2145bac748299f1e6e563
Dabeer Khan <dabeer.khan14@gmail.com>
2026-01-29 10:56:29 -0800

    Update Product.java

--- src/main/java/com/dabApp/InventoryManagement/product/entity/Product.java
+++ src/main/java/com/dabApp/InventoryManagement/product/entity/Product.java
@@ -1,5 +1,6 @@
 package com.dabApp.InventoryManagement.product.entity;
 
+import com.dabApp.InventoryManagement.business.entity.Business;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Positive;
 import org.hibernate.annotations.CreationTimestamp;
@@ -8,33 +9,92 @@ import java.math.BigDecimal;
 import java.time.Instant;
 
 @Entity
-@Table(name = "products")
+@Table(
+        name = "products",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"business_id","sku"})
+        }
+)
 public class Product
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "name" , nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "sku", nullable = false)
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "id" , referencedColumnName = "id")
-    private String sku;
-
-    @CreationTimestamp
-    @Column(name = "created_at" ,nullable = false , updatable = false)
-    private Instant createdAt;
-
-    @Column(name = "price")
+    @Column(name = "price", nullable = false , precision = 10 , scale = 2)
     private BigDecimal price;
 
-    @Column(name = "is_deleted")
-    private boolean isDeleted = false;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "business_id" , nullable = false)
+    private Business business;
 
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
 
-    //constructors
-    //getter and setters
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted =false;
 
+    public Product() {
+    }
+
+    public Product(long id, String name, BigDecimal price, Business business, Instant createdAt, boolean isDeleted) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.business = business;
+        this.createdAt = createdAt;
+        this.isDeleted = isDeleted;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Business getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(Business business) {
+        this.business = business;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
 }
