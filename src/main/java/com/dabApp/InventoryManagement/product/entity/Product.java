9ae5538aaa8f7ae0c0656d60e12bf534e9d1af3e
Dabeer Khan <Dabeer.khan3014@gmail.com>
2026-02-05 12:57:07 -0800

    Remove code product and stock

--- src/main/java/com/dabApp/InventoryManagement/product/entity/Product.java
+++ src/main/java/com/dabApp/InventoryManagement/product/entity/Product.java
@@ -9,91 +9,9 @@ import java.time.Instant;
 
 @Entity
 @Table(
-        name = "products",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"business_id","sku"})
-        }
+        name = "products"
 )
 public class Product
 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "price", nullable = false , precision = 10 , scale = 2)
-    private BigDecimal price;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "business_id" , nullable = false)
-    private Business business;
-
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private Instant createdAt;
-
-    @Column(name = "is_deleted", nullable = false)
-    private boolean isDeleted =false;
-
-    public Product() {
-    }
-
-    public Product(long id, String name, BigDecimal price, Business business, Instant createdAt, boolean isDeleted) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.business = business;
-        this.createdAt = createdAt;
-        this.isDeleted = isDeleted;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Business getBusiness() {
-        return business;
-    }
-
-    public void setBusiness(Business business) {
-        this.business = business;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
 }
--- src/main/java/com/dabApp/InventoryManagement/product/entity/Stock.java
+++ src/main/java/com/dabApp/InventoryManagement/product/entity/Stock.java
@@ -5,43 +5,8 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(
-        name = "stocks"
-//        @UniqueConstraint(stock_id)
-)
+        name = "stocks")
 public class Stock
 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false , unique = true)
-    private Product product;
-
-    @Column(name = "quantity", nullable = false)
-    private Integer quantity;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+    
 }
